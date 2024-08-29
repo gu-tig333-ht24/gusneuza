632f24de532ca8ec3f74c39068682718f1c6604f
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-08-29 21:26:38 +0200

    move todo samples to extension

--- lib/src/models/todo.dart
+++ lib/src/models/todo.dart
@@ -9,3 +9,16 @@ class Todo {
   final String title;
   final bool done;
 }
+
+extension TodoExtensions on Todo {
+  static List<Todo> samples = [
+    Todo(id: "1", title: "Write a book"),
+    Todo(id: "2", title: "Do homework"),
+    Todo(id: "3", title: "Tidy room", done: true),
+    Todo(id: "4", title: "Watch TV"),
+    Todo(id: "5", title: "Nap"),
+    Todo(id: "6", title: "Shop groceries"),
+    Todo(id: "7", title: "Have fun"),
+    Todo(id: "8", title: "Meditate"),
+  ];
+}
